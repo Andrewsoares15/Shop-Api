30c9a5d56e533f30a44ee7cbb7fdf71e42e7a965
Andrew soares <andrewsoares347@gmail.com>
2022-10-17 10:48:19 -0300

    shop fix: ajusta nome dos topicos

--- shop-validator/src/main/java/br/com/shop/validator/producer/SendTopicValidateShopSuccess.java
+++ shop-validator/src/main/java/br/com/shop/validator/producer/SendTopicValidateShopSuccess.java
@@ -14,7 +14,7 @@ public class SendTopicValidateShopSuccess{
     @Autowired
     private KafkaTemplate<String, ShopDTO> kafkaTemplate;
 
-    private final static String ShopDTO_TOPIC_VALIDATE_NAME = "TOPIC_ShopDTO_VALIDATE";
+    private final static String ShopDTO_TOPIC_VALIDATE_NAME = "TOPIC_SHOP_VALIDATE";
 
     public void send(ShopDTO ShopDTO) {
         log.info("m=send, ShopDTO validate with success, status {}", ShopDTO.getStatus());
